5de2857308c37aa5d02358e427c2960d43a8094b
morven2018 <you@example.com>
2025-02-21 01:28:41 +0300

    feat: fromJSON task added

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -236,8 +236,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const data = JSON.parse(json);
+  const res = Object.create(proto);
+  Object.assign(res, data);
+  return res;
 }
 
 /**
